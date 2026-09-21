19da3373db3818e06b4e6c8295bd8d73aa0c70c4
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2026-09-21 10:20:39 +0530

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} details
+     * @return {number}
+     */
+    countSeniors(details: string[]): number {
+        let sixty_and_more = 0;
+
+        for(let i = 0 ;i <details.length;i++){
+            let age = +(details[i][11]+details[i][12])
+            if(age>60){
+                sixty_and_more++
+            }
+        }
+
+        return sixty_and_more;
+
+    }
+}
