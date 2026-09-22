57cfa17d14056a21921f17794547e4d03a5cafb9
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2026-09-22 11:22:36 +0530

    Add: find-words-that-can-be-formed-by-characters - submission-7

---
+++ Data Structures & Algorithms/find-words-that-can-be-formed-by-characters/submission-7.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @param {string} chars
+     * @return {number}
+     */
+    countCharacters(words: string[], chars: string): number {
+        let good_count = 0;
+        let chars_arr = chars.split('')
+        for(const i of words){
+            let check = true;
+            // console.log(i)
+            let check_against = structuredClone(chars_arr)
+            for(const j of i){
+                
+                if(check_against.includes(j)==false){
+                    check = false 
+                    break
+                }
+                // check_against = check_against.map(x => x === j ? '_' : x);
+                let replace_for = check_against.indexOf(j)
+                check_against[replace_for]='_'
+                // console.log(check_against)
+            }
+            // console.log(check)
+            if(check){
+                // check_against.replace()
+                good_count+=i.length
+            }
+
+        }
+
+        return good_count;
+    }
+}
